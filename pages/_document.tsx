dfb68ba2696eeb0d84adfc1ffbbcabdaeac093b7
seunghyonpark <creath.park@gmail.com>
2023-05-30 18:47:58 +0900

    og update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,20 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:site_name" content="GRANDERBY"></meta>
+          <meta property="og:title" content="GRANDERBY"></meta>
+          <meta property="og:description" content="powered by MOMOCON"></meta>
+          <meta property="og:image" content="/intro-bg.png"></meta>
+          <meta property="og:image:width" content="1400"></meta>
+          <meta property="og:image:height" content="1400"></meta>
+
+          <meta name="twitter:card" content="summary_large_image"></meta> 
+          <meta name="twitter:image" content="/intro-bg.png"></meta>
+
+
+
             <link rel="icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
